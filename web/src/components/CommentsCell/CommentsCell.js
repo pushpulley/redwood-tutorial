f8c34f7b83b08d107075df55929ff8c62e0f8724
Will Hoffmann <whoffx@gmail.com>
2021-02-22 18:18:48 +0100

    Add Comments cell

---
+++ web/src/components/CommentsCell/CommentsCell.js
@@ -0,0 +1,17 @@
+export const QUERY = gql`
+  query CommentsQuery {
+    comments {
+      id
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const Success = ({ comments }) => {
+  return JSON.stringify(comments)
+}
