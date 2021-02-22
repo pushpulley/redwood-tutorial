87b53d11eecf1d79dc40138f0407ef0e97220870
Will Hoffmann <whoffx@gmail.com>
2021-02-22 18:53:49 +0100

    Add CommentForm storybook

--- web/src/components/CommentForm/CommentForm.stories.js
+++ web/src/components/CommentForm/CommentForm.stories.js
@@ -1,7 +1,24 @@
 import CommentForm from './CommentForm'
 
 export const generated = () => {
-  return <CommentForm />
+  mockGraphQLMutation('CreateCommentMutation', (variables, { ctx }) => {
+    const id = parseInt(Math.random() * 1000)
+    ctx.delay(1000)
+    return {
+      comment: {
+        id,
+        name: variables.input.name,
+        body: variables.input.body,
+        createdAt: new Date().toISOString(),
+      },
+    }
+  })
+
+  return (
+    <div className="m-4">
+      <CommentForm />
+    </div>
+  )
 }
 
 export default { title: 'Components/CommentForm' }
